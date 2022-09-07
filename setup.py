cae543ad413da034109c16d0e0b74a2dd8273668
13879034_admin <13879034_admin@UTS038758.adsroot.uts.edu.au>
2022-09-07 17:26:20 +1000

    New setup.py to install all packages with find_packages function

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 # Declare variables for the setup function
@@ -6,7 +6,7 @@ PROJECT_NAME = "Housing Predictor"
 VERSION = "0.0.1"
 AUTHOR = "Eric Tran"
 DESCRIPTION = "This is an End-to-end Machine Learning Project"
-PACKAGES = ["housing"]
+# PACKAGES = ["housing"]
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
 
@@ -16,14 +16,16 @@ def get_requirements_list() -> List[str]:
     Return: List of required libraries in requirements.txt
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_files:
-        return requirement_files.readlines()
+        return requirement_files.readlines().remove("-e .")
+        #return requirement_files.readlines()
 
 setup (
     name = PROJECT_NAME,
     version = VERSION,
     author = AUTHOR,
     description = DESCRIPTION,
-    packages = PACKAGES,
+    # packages = PACKAGES,
+    packages = find_packages(), # packages here is "housing" that contains __init__.py file
     install_requires = get_requirements_list(),
 )
 
